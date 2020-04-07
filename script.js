9c3e4f36050dd0380702e8e7ca297176619aaba7
Krix <alexey.2k04@gmail.com>
2020-04-08 00:05:41 +0300

    init

---
+++ script.js
@@ -0,0 +1,323 @@
+const keysEn = 
+'<div class="key" data-code="Backquote">`</div>   '+`
+<div class="key" data-code="Digit1">1</div>
+<div class="key" data-code="Digit2">2</div>
+<div class="key" data-code="Digit3">3</div>
+<div class="key" data-code="Digit4">4</div>
+<div class="key" data-code="Digit5">5</div>
+<div class="key" data-code="Digit6">6</div>
+<div class="key" data-code="Digit7">7</div>
+<div class="key" data-code="Digit8">8</div>
+<div class="key" data-code="Digit9">9</div>
+<div class="key" data-code="Digit0">0</div>
+<div class="key" data-code="Minus">-</div>
+<div class="key" data-code="Equal">=</div>
+<div class="key hot" data-code="Backspace">Backspace</div>
+<div class="key hot" data-code="Tab">Tab</div>
+<div class="key" data-code="KeyQ">q</div>
+<div class="key" data-code="KeyW">w</div>
+<div class="key" data-code="KeyE">e</div>
+<div class="key" data-code="KeyR">r</div>
+<div class="key" data-code="KeyT">t</div>
+<div class="key" data-code="KeyY">y</div>
+<div class="key" data-code="KeyU">u</div>
+<div class="key" data-code="KeyI">i</div>
+<div class="key" data-code="KeyO">o</div>
+<div class="key" data-code="KeyP">p</div>
+<div class="key" data-code="BracketLeft">[</div>
+<div class="key" data-code="BracketRight">]</div> 
+<div class="key" data-code="Backslash">\</div> 
+<div class="key hot" data-code="Delete">DEL</div>
+<div class="key hot" data-code="CapsLock">Caps Lock</div>
+<div class="key" data-code="KeyA">a</div>
+<div class="key" data-code="KeyS">s</div>
+<div class="key" data-code="KeyD">d</div>
+<div class="key" data-code="KeyF">f</div>
+<div class="key" data-code="KeyG">g</div>
+<div class="key" data-code="KeyH">h</div>
+<div class="key" data-code="KeyJ">j</div>
+<div class="key" data-code="KeyK">k</div>
+<div class="key" data-code="KeyL">l</div>
+<div class="key" data-code="Semicolon">;</div>
+<div class="key" data-code="Quote">'</div>
+<div class="key hot" data-code="Enter">ENTER</div>
+<div class="key hot" data-code="ShiftLeft">Shift</div>
+<div class="key" data-code="KeyZ">z</div>
+<div class="key" data-code="KeyX">x</div>
+<div class="key" data-code="KeyC">c</div>
+<div class="key" data-code="KeyV">v</div>
+<div class="key" data-code="KeyB">b</div>
+<div class="key" data-code="KeyN">n</div>
+<div class="key" data-code="KeyM">m</div>
+<div class="key" data-code="Period">.</div>
+<div class="key" data-code="Comma">,</div>
+<div class="key" data-code="Slash">/</div>
+<div class="key hot" data-code="ArrowUp">△</div>
+<div class="key hot" data-code="ShiftRight">Shift</div>
+<div class="key hot" data-code="ControlLeft">Ctrl</div>
+<div class="key hot" data-code="MetaLeft">Meta</div>
+<div class="key hot" data-code="AltLeft">Alt</div>
+<div class="key" data-code="Space"></div>
+<div class="key hot" data-code="AltRight">Alt</div>
+<div class="key hot" data-code="ControlRight">Ctrl</div>
+<div class="key hot" data-code="ArrowLeft">◁</div>
+<div class="key hot" data-code="ArrowDown">▽</div>
+<div class="key hot" data-code="ArrowRight">▷</div>
+`;
+
+const keysEnCaps = `
+<div class="key" data-code="Backquote">~</div>
+<div class="key" data-code="Digit1">!</div>
+<div class="key" data-code="Digit2">@</div>
+<div class="key" data-code="Digit3">#</div>
+<div class="key" data-code="Digit4">$</div>
+<div class="key" data-code="Digit5">%</div>
+<div class="key" data-code="Digit6">^</div>
+<div class="key" data-code="Digit7">&</div>
+<div class="key" data-code="Digit8">*</div>
+<div class="key" data-code="Digit9">(</div>
+<div class="key" data-code="Digit0">)</div>
+<div class="key" data-code="Minus">_</div>
+<div class="key" data-code="Equal">+</div>
+<div class="key hot" data-code="Backspace">Backspace</div>
+<div class="key hot" data-code="Tab">Tab</div>
+<div class="key" data-code="KeyQ">Q</div>
+<div class="key" data-code="KeyW">W</div>
+<div class="key" data-code="KeyE">E</div>
+<div class="key" data-code="KeyR">R</div>
+<div class="key" data-code="KeyT">T</div>
+<div class="key" data-code="KeyY">Y</div>
+<div class="key" data-code="KeyU">U</div>
+<div class="key" data-code="KeyI">I</div>
+<div class="key" data-code="KeyO">O</div>
+<div class="key" data-code="KeyP">P</div>
+<div class="key" data-code="BracketLeft">{</div>
+<div class="key" data-code="BracketRight">}</div>
+<div class="key" data-code="Backslash">|</div>
+<div class="key hot" data-code="Delete">DEL</div>
+<div class="key hot" data-code="CapsLock">Caps Lock</div>
+<div class="key" data-code="KeyA">A</div>
+<div class="key" data-code="KeyS">S</div>
+<div class="key" data-code="KeyD">D</div>
+<div class="key" data-code="KeyF">F</div>
+<div class="key" data-code="KeyG">G</div>
+<div class="key" data-code="KeyH">H</div>
+<div class="key" data-code="KeyJ">J</div>
+<div class="key" data-code="KeyK">K</div>
+<div class="key" data-code="KeyL">L</div>
+<div class="key" data-code="Semicolon">:</div>
+<div class="key" data-code="Quote">"</div>
+<div class="key hot" data-code="Enter">ENTER</div>
+<div class="key hot" data-code="ShiftLeft">Shift</div>
+<div class="key" data-code="KeyZ">Z</div>
+<div class="key" data-code="KeyX">X</div>
+<div class="key" data-code="KeyC">C</div>
+<div class="key" data-code="KeyV">V</div>
+<div class="key" data-code="KeyB">B</div>
+<div class="key" data-code="KeyN">N</div>
+<div class="key" data-code="KeyM">M</div>
+<div class="key" data-code="Period">></div>
+<div class="key" data-code="Comma"><</div>
+<div class="key" data-code="Slash">?</div>
+<div class="key hot" data-code="ArrowUp">△</div>
+<div class="key hot" data-code="ShiftRight">Shift</div>
+<div class="key hot" data-code="ControlLeft">Ctrl</div>
+<div class="key hot" data-code="MetaLeft">Meta</div>
+<div class="key hot" data-code="AltLeft">Alt</div>
+<div class="key" data-code="Space"></div>
+<div class="key hot" data-code="AltRight">Alt</div>
+<div class="key hot" data-code="ControlRight">Ctrl</div>
+<div class="key hot" data-code="ArrowLeft">◁</div>
+<div class="key hot" data-code="ArrowDown">▽</div>
+<div class="key hot" data-code="ArrowRight">▷</div>
+`;
+
+const body = document.querySelector('body');
+const keyboard = document.createElement('div');
+keyboard.classList = 'kboard';
+const textview = document.createElement('textarea');
+textview.classList = 'textview';
+
+body.appendChild(textview);
+body.appendChild(keyboard);
+
+let caps = false;
+
+const capsEn = () =>  {
+  if (caps === false) {
+    caps = true;
+    keyboard.innerHTML = keysEnCaps;
+    document.querySelector('.key[data-code="CapsLock"]').classList.add('active');
+    return;
+  }
+  if (caps === true) {
+    caps = false;
+    keyboard.innerHTML = keysEn;
+    document.querySelector('.key[data-code="CapsLock"]').classList.add('active');
+    return;
+  }
+}
+
+const shiftEn = () =>  {
+  if (caps === false) {
+    keyboard.innerHTML = keysEnCaps;
+    return;
+  }
+  if (caps === true) {
+    keyboard.innerHTML = keysEn;
+    return;
+  }
+}
+
+const getCaret = () => { 
+  if (textview.selectionStart) { 
+    return textview.selectionStart; 
+  } else if (document.selection) { 
+    textview.focus(); 
+    let r = document.selection.createRange(); 
+    if (r == null) return 0; 
+    let re = textview.createTextRange(), 
+        rc = re.duplicate(); 
+    re.moveToBookmark(r.getBookmark()); 
+    rc.setEndPoint('EndToStart', re); 
+    return rc.text.length; 
+  }  
+  return 0; 
+}
+
+const backspacePressed = () => {
+  let pos = getCaret();
+  if (pos === 0) return;
+  textview.value = textview.value.substring(0, pos-1) +
+   textview.value.substring(pos);
+  textview.setSelectionRange(pos-1, pos-1);
+  textview.focus();
+}
+const delPressed = () => {
+  let pos = getCaret();
+  textview.value = textview.value.substring(0, pos) +
+   textview.value.substring(pos+1);
+  textview.setSelectionRange(pos, pos);
+  textview.focus();
+}
+
+//                  MOUSE
+keyboard.addEventListener('mousedown', e => { 
+  if (e.target.classList.value.indexOf('key') !== -1) {
+    e.target.classList.add('active');
+    
+    if (e.target.dataset.code == 'Delete') {
+      delPressed();
+    }
+    if (e.target.dataset.code == 'Backspace') {
+      backspacePressed();
+    } else if (e.target.dataset.code == 'Space') {
+      textview.value += ' ';
+    } else
+    if (e.target.classList.value.indexOf('hot') == -1) {
+      textview.value += e.target.textContent; 
+    }
+    if (e.target.dataset.code === 'CapsLock') {
+      e.target.classList.add('active');
+    }
+    if (e.target.dataset.code == 'CapsLock') {
+      capsEn();
+    }
+    if (e.target.dataset.code == 'ShiftLeft' ||
+        e.target.dataset.code == 'ShiftRight') {
+      shiftEn();
+    }
+  }
+});
+
+keyboard.addEventListener('mouseup', e => {
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => {
+    key.classList.remove('active');
+  });
+
+  if (caps === true) {
+    keys.forEach(key => {
+      if (key.dataset.code === 'CapsLock') {
+        key.classList.add('active');
+      }
+    });
+  }
+  if (caps !== false) {
+    keyboard.innerHTML = keysEnCaps;
+    return;
+  }
+  if (caps !== true) {
+    keyboard.innerHTML = keysEn;
+    return;
+  }
+});
+///           KEYBOARD
+let keyTime;
+document.querySelector('.textview').addEventListener('keydown', e => {
+  e.preventDefault();
+});
+
+const keydown = e => {
+  if (e.code == 'Tab') {
+    e.preventDefault();
+  }
+    
+  const keys = document.querySelectorAll('.key');
+  let capsElem;
+  keys.forEach(key => {
+    if (key.dataset.code == e.code) {
+      key.classList.add('active');
+      if (key.classList != 'key hot active') {
+        textview.value += key.textContent;
+      }
+      if (key.dataset.code == 'CapsLock') {
+        capsElem = key;
+      }
+    }
+  });
+
+  if (e.code == 'CapsLock') {
+    capsElem.classList.add('active');
+  }
+  if (e.code == 'Backspace') {
+    backspacePressed();
+  }
+  if (e.code == 'Delete') {
+    delPressed();
+  }
+  if (e.code == 'CapsLock') capsEn();
+  if (e.code == 'ShiftLeft' || e.code == 'ShiftRight') {
+    shiftEn();
+  }
+}
+
+document.addEventListener('keydown', e => {
+  keydown(e);  
+});
+
+document.addEventListener('keyup', e => {
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => {
+    key.classList.remove('active');
+  });
+
+  if (caps === true) {
+    keys.forEach(key => {
+      if (key.dataset.code === 'CapsLock') {
+        key.classList.add('active');
+      }
+    });
+  }
+
+  if (caps !== false) {
+    keyboard.innerHTML = keysEnCaps;
+    return;
+  }
+  if (caps !== true) {
+    keyboard.innerHTML = keysEn;
+    return;
+  }
+});
+
+keyboard.innerHTML = keysEn;
